95e9b234f19c3236b6ea2ee11d75fa0ff60b2bec
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-02-22 17:29:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,15 @@ def map_to_negativize(source_array)
   end
   my_array
 end
+
+
+ def map_to_no_change(source_array) 
+   index = 0
+   my_array = []
+   while index < source_array.length do
+     my_array << source_array[index]
+     index += 1
+   end
+   my_array
+   
+ end
